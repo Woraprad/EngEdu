0f746b154096ca9250ec244061adf6904a91e17d
Woraprad <58030217@kmitl.ac.th>
2017-10-19 01:08:33 +0700

    2nd  year semester 2

--- EngEdu/EngEdu/Program.cs
+++ EngEdu/EngEdu/Program.cs
@@ -13,6 +13,7 @@ namespace EngEdu
             Console.WriteLine("Year 255x semester 1, I am an EngEdu freshman");
             Console.WriteLine("Year 255x semester 2, I lean to program in c sharp");
             Console.WriteLine("Year 2560 semester 1, I start my program in life");
+            Console.WriteLine("Year 2560 semester 2, I lean to summer");
         }
     }
 }
