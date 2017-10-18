d5e9fae771cd593c3c739b1e5b5a4c19807ee086
Woraprad <58030217@kmitl.ac.th>
2017-10-19 01:20:40 +0700

    4th  year semester 1

--- EngEdu/EngEdu/Program.cs
+++ EngEdu/EngEdu/Program.cs
@@ -16,6 +16,7 @@ namespace EngEdu
             Console.WriteLine("Year 2560 semester 2, I lean to summer");
             Console.WriteLine("Year 2561 semester 1, I lean to OOAD");
             Console.WriteLine("Year 2561 semester 2, I lean to Industrial Training");
+            Console.WriteLine("Year 2562 semester 1, I lean to be teacher");
         }
     }
 }
